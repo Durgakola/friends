ba877c02f6583bd845f338c4f58e3dd8214c7d3c
Durgakola <Durgakola50@gmail.com>
2024-02-13 11:41:50 +0530

    add

--- src/Addfriends.java
+++ src/Addfriends.java
@@ -2,4 +2,8 @@ public class Addfriends {
     public void updatefri(){
         System.out.println("3 friends update");
     }
+    public void addition(){
+        System.out.println(a+b);
+    }
+
 }
